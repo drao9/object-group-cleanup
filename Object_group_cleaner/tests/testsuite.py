cf76c2b48c9473150b6487e5d31f95b5a29aba8e
axperez <axperez@cisco.com>
2017-08-02 13:41:24 -0700

    Testing performance limit

--- Object_group_cleaner/tests/testsuite.py
+++ Object_group_cleaner/tests/testsuite.py
@@ -186,7 +186,7 @@ class TestOGC(unittest.TestCase):
                     for ogtyp in root.devices.device[constants.device_name].config.asa__object_group:
                         og_num = og + str(num_types) + '_'
                         num_types = num_types + 1
-                        for j in range(5000):
+                        for j in range(55000):
                             fake_og = og_num + str(j)
                             root.devices.device[constants.device_name].config.asa__object_group[ogtyp].create(fake_og)
 
@@ -202,7 +202,7 @@ class TestOGC(unittest.TestCase):
                         acl_num = holder + str(i)
                         root.devices.device[constants.device_name].config.asa__access_list.access_list_id.create(acl_num)
                         rul_num = rul + str(i) + '_'
-                        for j in range(4500):
+                        for j in range(50000):
                             fake_rule = rul_num + str(j)
                             root.devices.device[constants.device_name].config.asa__access_list.access_list_id[acl_num].rule.create(fake_rule)
 
