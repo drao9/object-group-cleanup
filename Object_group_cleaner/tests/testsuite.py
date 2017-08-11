de5df24a41b3860314c75b7707cd7c79682d61fe
axperez <axperez@cisco.com>
2017-08-11 10:43:32 -0700

    testsuite performance edit

--- Object_group_cleaner/tests/testsuite.py
+++ Object_group_cleaner/tests/testsuite.py
@@ -8,7 +8,7 @@ class TestOGC(unittest.TestCase):
     """
     This program tests if the Object_group_cleaner tool is able to search all the object group list and checks if there are object groups that need to be deleted.
     """
-
+    """
     def test_search_empty(self):
 
         #With this test case, we create a netsim such that none of the object groups need to be deteled. This test passes if no object groups are returned.
@@ -192,7 +192,7 @@ class TestOGC(unittest.TestCase):
                         del root.devices.device[constants.device_name].config.asa__access_list.access_list_id[acl.id]
 
                     t.apply()
-
+    """
     def test_perform(self):
 
         #With this test case, we create a netsim with 5000 object groups and 4500 ACL lines and checked if the tool runs within 500 secs.
@@ -227,7 +227,7 @@ class TestOGC(unittest.TestCase):
                     for ogtyp in root.devices.device[constants.device_name].config.asa__object_group:
                         og_num = og + str(num_types) + '_'
                         num_types = num_types + 1
-                        for j in range(14000):
+                        for j in range(25000):
                             fake_og = og_num + str(j)
                             root.devices.device[constants.device_name].config.asa__object_group[ogtyp].create(fake_og)
 
@@ -243,7 +243,7 @@ class TestOGC(unittest.TestCase):
                         acl_num = holder + str(i)
                         root.devices.device[constants.device_name].config.asa__access_list.access_list_id.create(acl_num)
                         rul_num = rul + str(i) + '_'
-                        for j in range(13970):
+                        for j in range(24970):
                             fake_rule = rul_num + str(j)
                             root.devices.device[constants.device_name].config.asa__access_list.access_list_id[acl_num].rule.create(fake_rule)
 
@@ -280,7 +280,7 @@ class TestOGC(unittest.TestCase):
                         del root.devices.device[constants.device_name].config.asa__access_list.access_list_id[acl.id]
 
                     t.apply()
-
+    """
     def test_remove(self):
 
         #With this test case, we create a netsim such that all of the object groups need to be deteled. This test passes if all object groups are returned and there are none left.
@@ -370,7 +370,7 @@ class TestOGC(unittest.TestCase):
                         del root.devices.device[constants.device_name].config.asa__access_list.access_list_id[acl.id]
 
                     t.apply()
-
+    """
 
 if __name__ == '__main__':
     unittest.main()
