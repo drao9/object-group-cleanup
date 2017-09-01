0d9323e7a956af9c980846a869e6a48a17acc187
axperez <axperez@cisco.com>
2017-09-01 11:57:20 -0700

    Debugging

--- Object_group_cleaner/tests/modules/__init__.py
+++ Object_group_cleaner/tests/modules/__init__.py
@@ -29,7 +29,7 @@ def init_logger():
 def importLibs(packageName):
 	currentDir = os.getcwd()
 	oldDir = currentDir
-	currentDir = currentDir + os.sep + "packages" + os.sep + packageName + os.sep + "python" + os.sep + "modules" + os.sep
+	currentDir = currentDir + os.sep + "packages" + os.sep + packageName + os.sep + "tests" + os.sep + "modules" + os.sep
 	for moduleNames in os.listdir(currentDir):
 		os.chdir(currentDir)
 		if fnmatch.fnmatch(moduleNames, '*_module.py'):
